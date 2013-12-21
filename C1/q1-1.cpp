01db4c27b44554958aa986d6e40040042ce247d6
36V3mA21Hz <ahzjx1989@gmail.com>
2013-12-21 17:49:41 -0500

    Finally

--- C1/q1-1.cpp
+++ C1/q1-1.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
-
+#include <string>
+using namespace std;
+class DetermineString
+{
+    string str;
+public:
+    DetermineString(string str) {this->str = str;};
+    bool DetermineUnique()
+    {
+        char letters[256] = {};
+        for (int i = 0; i < str.length(); i++)
+        {
+            int j = (int)str[i];
+            if (letters[j]) return false;
+            else letters[j] = true;
+        }
+        return true;
+    };
+};
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+    string s0 ("lqwerqtyu.");
+    DetermineString d0(s0);
+    cout << d0.DetermineUnique() << endl;
+    return 0;    
 }
+
