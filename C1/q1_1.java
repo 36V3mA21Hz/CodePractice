dbd39b05231dce4d54f6a89cea44c3ab7b977d5f
36V3mA21Hz <ahzjx1989@gmail.com>
2013-12-25 16:06:24 -0500

    changed

--- C1/q1_1.java
+++ C1/q1_1.java
@@ -1,17 +1,21 @@
 public class q1_1 {
     public static int[] NonZeros(int[] A) {
-        int [] B;
+        int nonz = 0;
+        for (int i=0; i < A.length; i++) 
+            if ( A[i]!=0) nonz++;
+        int [] B = new int [nonz];
         int j = 0;
         for (int i=0; i < A.length; i++) {
             if ( A[i] != 0) {
-                int B[j] = A[i];
+                B[j] = A[i];
                 j++;
             }        
         }
+    return B;
     }     
-    public static void main() {
+    public static void main(String[] args) {
         int [] A = {1, 22, 0};
         int [] B = NonZeros(A);
-        for (int x in B) System.out.println(x);
+        for (int i = 0; i < B.length; i++) System.out.print(B[i] + ", ");
     }
 }
