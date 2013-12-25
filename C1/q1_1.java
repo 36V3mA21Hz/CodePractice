94d80b4a8492396196169803f2f1e88a90003dde
36V3mA21Hz <ahzjx1989@gmail.com>
2013-12-25 17:37:35 -0500

    final

--- C1/q1_1.java
+++ C1/q1_1.java
@@ -1,18 +1,21 @@
-public class q1_1 {
-    public boolean determine_Unique ( string [] A ) {
+class q1_1 {
+    public static boolean determine(String A) {
         int B [];
         B = new int [256];
-        for (char c : A) {
-            if (B[(int)c] == 1)
+        for (int i = 0; i < A.length(); i ++) {
+            int c = A.charAt(i);
+            if (B[c] == 1)
                 return false;
             else
-                B[(int)c] = 1;
+                B[c] = 1;
         }
         return true;
     }
     
     public static void main(String [] args) {
-        System.out.println( dertermine_Unique(args[1]));
+        String A = args[0];
+        boolean x = determine(A);
+        System.out.println(x);
     }
 }
 
